0d76c66e62f7d308f19d736f502e4fbd9e8ff5f3
Brian Bolnick <brianbolnick@gmail.com>
2024-05-03 08:41:25 -0600

    update exp

--- src/components/App.js
+++ src/components/App.js
@@ -1,40 +1,49 @@
-import React, { Component } from 'react';
-import '../css/App.css';
-import Home from './Home/HomePage.js';
-import NotFound from './NotFound.js';
-import AuthError from './AuthError.js';
-import Destinder from './Destinder.js';
-import SellOut from './Sellout.js';
-import Icpm from './Icpm.js';
-import Gilagoat from './Gilagoat.js';
-import About from './About.js';
+import React from "react";
+import "../css/App.css";
+import Home from "./Home/HomePage.js";
+import NotFound from "./NotFound.js";
+import AuthError from "./AuthError.js";
+import Destinder from "./Destinder.js";
+import SellOut from "./Sellout.js";
+import Icpm from "./Icpm.js";
+import Gilagoat from "./Gilagoat.js";
+import About from "./About.js";
 // import Spin from './SpinDemo.js';
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
-import { Route, withRouter, Switch } from 'react-router-dom';
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+import { Route, Routes, useLocation } from "react-router-dom";
 
-class App extends Component {
-  render() {
-    const currentKey = this.props.location.pathname.split('/')[1] || '/';
-    const timeout = { enter: 300, exit: 200 };
-    return (
+const App = () => {
+  const location = useLocation();
+  const currentKey = location.pathname.split("/")[1] || "/";
+  const timeout = { enter: 300, exit: 200 };
 
-      <TransitionGroup component="main" className="page-main" style={{ height: '100%' }}>
-        <CSSTransition key={currentKey} timeout={timeout} classNames="fade" appear>
-          <Switch location={this.props.location}>
-            <Route path="/" exact component={Home} />
-            <Route path="/destinder" exact component={Destinder} />
-            <Route path="/sellout" exact component={SellOut} />
-            <Route path="/icpm" exact component={Icpm} />
-            <Route path="/gilagoat" exact component={Gilagoat} />
-            <Route path="/about" exact component={About} />
-            {/* <Route path="/spin-demo" exact component={Spin} /> */}
-            <Route path="/auth_error" exact component={AuthError} />
-            <Route component={NotFound} />
-          </Switch>
-        </CSSTransition>
-      </TransitionGroup>
-    )
-  }
-}
+  return (
+    <TransitionGroup
+      component="main"
+      className="page-main"
+      style={{ height: "100%" }}
+    >
+      <CSSTransition
+        key={currentKey}
+        timeout={timeout}
+        classNames="fade"
+        appear
+      >
+        <Routes location={location}>
+          <Route path="/" element={<Home />} />
+          <Route path="/destinder" element={<Destinder />} />
+          <Route path="/sellout" element={<SellOut />} />
+          <Route path="/icpm" element={<Icpm />} />
+          <Route path="/gilagoat" element={<Gilagoat />} />
+          <Route path="/about" element={<About />} />
+          {/* <Route path="/spin-demo" element={<Spin />} /> */}
+          <Route path="/auth_error" element={<AuthError />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </CSSTransition>
+    </TransitionGroup>
+  );
+};
 
-export default withRouter(App)
+export default App;
+
